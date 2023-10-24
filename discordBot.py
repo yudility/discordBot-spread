0f6c63139fbc192609e20f464c47cc1ae4e66012
yudility <yudility@gmail.com>
2023-10-25 02:56:33 +0900

    development

--- discordBot.py
+++ discordBot.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-import spreadtest
+from spreadtest import *
 
 bot = commands.Bot(command_prefix='/',intents=discord.Intents.all())
 
@@ -22,6 +22,22 @@ async def hello(ctx):
 @bot.command()
 async def talk(ctx, arg1, arg2):
     await ctx.reply('{} talks to {}.'.format(arg1, arg2))
+
+@bot.command()
+async def addMe(ctx, arg1, arg2):
+    await ctx.reply('{} add {}.'.format(arg1, arg2))
+
+myRole = 'myRole'
+
+@bot.command()
+@commands.has_any_role(myRole)
+async def cool(ctx):
+    await ctx.send('You are cool indeed {}.'.format(myRole))
+    
+@bot.command()
+async def addPersonTest(ctx, arg1, arg2, arg3):
+    addPerson(arg1, arg2, arg3)
+    await ctx.send('참여 완료! 가문명: {} , 직업: {} , 전/각: {} '.format(arg1,arg2,arg3))
 
 bot.run(key)
 
--- spreadtest.py
+++ spreadtest.py
@@ -1,12 +1,65 @@
 import gspread
+from gspread_formatting import *
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope = 'https://spreadsheets.google.com/feeds'
 json = 'keys/dotspreed-0f70c3397201.json'
-credentials = ServiceAccountCredentials.from_json_keyfile_name(json, scope)
-gc = gspread.authorize(credentials)
 sheet_url = 'https://docs.google.com/spreadsheets/d/1vBaQpznNODv-vvIymOm1571s-XFBphwXKG_7mTf5Als/edit#gid=0'
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(json, scope)
+
+gc = gspread.authorize(credentials)
 doc = gc.open_by_url(sheet_url)
 worksheet = doc.worksheet('시트1')
-cell_data = worksheet.acell('A1').value
-print(cell_data)
+
+# cell_data = worksheet.acell('A1').value
+#test = worksheet.update_acell('b11',"input test")
+
+cell_range = ['B16:D50']
+
+# 범위만큼 가져오기
+status = worksheet.batch_get(cell_range)
+
+print(len(status[0]))
+
+def getPerson():
+    new_name_list = []
+    
+    for i in range(len(status[0])):
+        person_info = status[0][i]
+        
+        if not person_info:
+            continue  # 빈 문자열인 경우 스킵
+        # 아니면 추가
+        new_name_list.append(person_info)
+    # 결과 출력    
+    # print(new_name_list)
+    return new_name_list
+
+def addPerson(name, job, jg):
+    
+    new_value = getPerson()
+    
+    new_person = [name, job, jg]
+    
+    new_value.append(new_person)
+    
+    # TODO
+    # 여기서 문제가 나는 것 같음. 찾아보자
+    worksheet.update(cell_range, new_value)
+    
+    worksheet.format(cell_range, {
+    "horizontalAlignment": "CENTER",
+    "textFormat": {
+      "foregroundColor": {
+        "red": 1.0,
+        "green": 1.0,
+        "blue": 1.0
+      },
+      "fontSize": 12,
+      "bold": True
+     }})
+    
+
+    
+
