71de9ea2af6f0a02ffea0fd3e97764d018b7a062
Yujin <yudility@gmail.com>
2023-10-28 00:30:22 +0900

    Feat: 참여인원확인 및 거점일정확인 명령어 추가

--- discordBot.py
+++ discordBot.py
@@ -8,6 +8,9 @@ file = open("keys/key.txt",'r')
 key = file.read()
 file.close()
 
+dot_role = '돚거단'
+inst_role = '파본'
+
 @bot.event
 async def on_ready():
     print('Done')
@@ -27,17 +30,45 @@ async def talk(ctx, arg1, arg2):
 async def addMe(ctx, arg1, arg2):
     await ctx.reply('{} add {}.'.format(arg1, arg2))
 
-myRole = 'myRole'
+@bot.command()
+@commands.has_any_role(dot_role)
+async def cool(ctx):
+    await ctx.send('You are hot indeed {}.'.format(dot_role))
+
+     
+# @bot.command()
+# async def addPersonTest(ctx, arg1, arg2, arg3):
+#     addPersonToDot(arg1, arg2, arg3)
+#     await ctx.send('참여 완료! 가문명: {} , 직업: {} , 전/각: {} '.format(arg1,arg2,arg3))
+
+# @bot.command()
+# async def at(ctx, 요일, 닉네임, 직업, 전각):
+#     addtest(요일,닉네임, 직업, 전각)
+#     await ctx.send('참여 완료! 가문명: {} , 직업: {} , 전/각: {} '.format(요일,닉네임,직업,전각))
 
 @bot.command()
-@commands.has_any_role(myRole)
-async def cool(ctx):
-    await ctx.send('You are cool indeed {}.'.format(myRole))
+async def 참여신청(ctx, 닉네임, 직업, 전각):
     
+    addPersonToDot(닉네임, 직업, 전각)
+    await ctx.send('참여 완료! 가문명: {} , 직업: {} , 전/각: {} '.format(닉네임,직업,전각))
+
+
 @bot.command()
-async def addPersonTest(ctx, arg1, arg2, arg3):
-    addPerson(arg1, arg2, arg3)
-    await ctx.send('참여 완료! 가문명: {} , 직업: {} , 전/각: {} '.format(arg1,arg2,arg3))
+async def 참여인원확인(ctx, 요일):
+    await ctx.send('{} 거점 인원은 ', getNumberWeek(요일), '입니다.')
+
+
+
+@bot.command()
+async def 거점일정확인(ctx):
+    date_info = getDate()
+
+    values_as_strings = [str(value) for value in date_info.values()]
+
+    all_date = ' / '.join(values_as_strings)
+
+    await ctx.send('<이번주 거점 일정>\n :'+ all_date)
+    await ctx.send('참여 신청 명령어 - /참여신청 닉네임 직업 전각')
 
 bot.run(key)
 
